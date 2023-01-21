fe0fa7fd25a0c04cf7d02635ab17fdd0cc8e154b
Marco.Barreto <marco.agbarreto@gmail.com>
2023-01-21 14:40:31 -0600

    prepared files for gunicorn deploy

--- app.py
+++ app.py
@@ -81,4 +81,5 @@ def get_scale(width, height):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)  # Production
+    # app.run(debug=True)  # Development
